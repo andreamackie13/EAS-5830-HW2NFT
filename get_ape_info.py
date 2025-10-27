8def780d4c955e6337adb65694027f27d0be6bf5
Andrea Mackie <andreamackie11@gmail.com>
2025-10-27 14:49:49 -0500

    Add files via upload

---
+++ get_ape_info.py
@@ -0,0 +1,61 @@
+from web3 import Web3
+from web3.providers.rpc import HTTPProvider
+import requests
+import json
+
+bayc_address = "0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D"
+contract_address = Web3.to_checksum_address(bayc_address)
+
+# You will need the ABI to connect to the contract
+# The file 'abi.json' has the ABI for the bored ape contract
+# In general, you can get contract ABIs from etherscan
+# https://api.etherscan.io/api?module=contract&action=getabi&address=0xBC4CA0EdA7647A8aB7C2061c2E118A18a936f13D
+with open('ape_abi.json', 'r') as f:
+    abi = json.load(f)
+
+############################
+# Connect to an Ethereum node
+api_url = f"https://mainnet.infura.io/v3/41831f4982e74499b13d43b443f36666"  # YOU WILL NEED TO PROVIDE THE URL OF AN ETHEREUM NODE
+provider = HTTPProvider(api_url)
+web3 = Web3(provider)
+
+
+def get_ape_info(ape_id):
+    assert isinstance(ape_id, int), f"{ape_id} is not an int"
+    assert 0 <= ape_id, f"{ape_id} must be at least 0"
+    assert 9999 >= ape_id, f"{ape_id} must be less than 10,000"
+
+    data = {'owner': "", 'image': "", 'eyes': ""}
+
+    contract = web3.eth.contract(address=contract_address, abi = abi)
+    owner = contract.functions.ownerOf(ape_id).call()
+    token_URI = contract.functions.tokenURI(ape_id).call()
+
+    if token_URI.startswith("ipfs://"):
+      token_URI = token_URI.replace("ipfs://", "https://ipfs.io/ipfs/")
+
+    data_requests = requests.get(token_URI)
+    metadata = data_requests.json()
+
+    image = metadata.get("image", "")
+
+    eyes = ""
+    for i in metadata.get("attributes", []):
+      if i.get("trait_type") == "Eyes":
+        eyes = i.get("value", "")
+        break
+
+    data["owner"] = owner
+    data["image"] = image
+    data["eye"] = eyes
+
+
+    assert isinstance(data, dict), f'get_ape_info{ape_id} should return a dict'
+    assert all([a in data.keys() for a in
+                ['owner', 'image', 'eyes']]), f"return value should include the keys 'owner','image' and 'eyes'"
+    return data
+
+
+if __name__ == "__main__":
+  print("Ape", get_ape_info(1))
+
